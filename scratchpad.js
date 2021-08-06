ea5c83a4f64c34e7799af64c99ba791a685430f5
Michael de Vreeze <michaelv@uptrends.com>
2021-08-06 17:07:26 +0200

    Add reset button and styling

--- scratchpad.js
+++ scratchpad.js
@@ -1,6 +1,5 @@
 let prefix = "negative-label-filter";
 
-
 function addStyles() {
   const styleId = `${prefix}-style`;
   let style = document.getElementById(styleId);
@@ -16,18 +15,36 @@ function addStyles() {
       margin: 0 5px 5px;
     }
 
+    .${prefix}-list {
+      display: flex;
+      width: 260px;
+      flex-wrap: wrap;
+    }
     .${prefix}-list-item-clickable button {
       font-size: 10px;
       margin: 0;
       border: 0;
       padding: 5px;
+      width: 100%;
     }
 
     .${prefix}-list-item-clickable {
       float: left;
       padding: 3px;
+      flex-grow: 1;
     }
 
+    .card-label-darkgray {
+      background-color: darkgray;
+    }
+
+    .${prefix}-active {
+       box-shadow: inset 1px 1px 5px 0px #4f4545;
+    }
+
+    .card-label-darkgray.${prefix}-active {
+      box-shadow: none !important;
+    }
   `;
 }
 
@@ -79,7 +96,6 @@ function showCard(card) {
 
 function addLabelsToSubHeader(list){
   const labels = getUniqueLabelsInList(list);
-  console.log(labels);
   const header = list.querySelector('.list-header');
   let subHeader = list.querySelector(`.${prefix}-list-sub-header`);
 
@@ -108,28 +124,39 @@ function addLabelsToSubHeader(list){
 
     // Construct the label elements
     for (const label of labels) {
-      const button = document.createElement("button");
-      button.innerText = label.title;
-
-      // Add color class from trello
-      button.classList.add(label.colorClass);
-
-      const li = document.createElement('li');
-      li.classList.add(`${prefix}-list-item`, `${prefix}-list-item-clickable`);
-      li.appendChild(button);
-
-      ul.appendChild(li);
-
-      button.addEventListener('click', (event) => {
-        buttonClicked(list, label.title)
-        event.stopPropagation();
-      });
+      const button = createAndAddLabelElement(ul, list, label.title, label.colorClass);
     }
+
+    // Add reset button
+    createAndAddLabelElement(ul, list, "Reset", "card-label-darkgray");
 
     subHeader.appendChild(ul);
   }
 }
 
-function addResetLabel(){
-  
+const buttonActiveClass = `${prefix}-active`;
+function createAndAddLabelElement(ul, list, title, colorClass){
+  const button = document.createElement("button");
+  button.innerText = title;
+
+  // Add color class from trello
+  button.classList.add(colorClass);
+
+  const li = document.createElement('li');
+  li.classList.add(`${prefix}-list-item`, `${prefix}-list-item-clickable`);
+
+  li.appendChild(button);
+
+  ul.appendChild(li);
+
+  button.addEventListener('click', (event) => {
+
+    buttonClicked(list, title)
+    Array.from(list.querySelectorAll(`.${buttonActiveClass}`)).forEach((i) => {
+      i.classList.remove(buttonActiveClass)
+    });
+    button.classList.add(buttonActiveClass);
+    event.stopPropagation();
+  });
 }
+
